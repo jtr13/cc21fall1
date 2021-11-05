7960fb5cdc6a758ee7eea5343b2cedb294caa4da
0506Zhengyi <zf2238@columbia.edu>
2021-11-06 02:42:13 +0800

    example dashbaord and constants codes

---
+++ resources/intro_to_flexdashboard_shiny/constants.R
@@ -0,0 +1,18 @@
+id.name <- "User ID"
+age.name <- "Age"
+gender.name <- "Gender"
+income.name <- "Income"
+region.name <- "Region"
+persona.name <- "Persona"
+
+product.name <- "Product"
+awareness.name <- "Awareness"
+consideration.name <- "Consideration"
+consumption.name <- "Consumption"
+satisfaction.name <- "Satisfaction"
+advocacy.name <- "Advocacy"
+
+bp.pattern <- "BP_"
+
+age.group.name <- "Age Group"
+income.group.name <- "Income Group"
